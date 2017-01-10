18868852da8ce2b4b73901901c2d67dc4e68d04a
marlopez04 <marlopez04@hotmail.com>
2017-01-10 19:48:33 -0300

    modulo 2 terminado

--- modulo2/platforms/browser/www/js/camara.js
+++ modulo2/platforms/browser/www/js/camara.js
@@ -1,21 +1,41 @@
 var app = {
 	inicio: function(){
 		this.iniciaFastClick();
-		this.iniciaBoton();
+		this.iniciaBotons();
 	},
 
 	iniciaFastClick: function(){
 		FastClick.attach(document.body);
 	},
 
-	iniciaBoton: function() {
+	iniciaBotons: function() {
 		var buttonAction = document.querySelector('#button-action');
-		buttonAction.addEventListener('click', this.tomarFoto);
+		buttonAction.addEventListener('click', function(){
+			app.cargarFoto(camera.PictureSourceType.CAMERA);
+		});
+
+		var filterButtons = document.querySelectorAll('.button-filter');
+		filterButtons[0].addEventListener('click', function (){
+			app.aplicaFiltro('gray');
+		});
+		filterButtons[1].addEventListener('click', function (){
+			app.aplicaFiltro('negative');
+		});
+		filterButtons[2].addEventListener('click', function (){
+			app.aplicaFiltro('sepia');
+		});
+
+		var buttonGallery = document.querySelector('#button-gallery'):
+		buttonGallery.addEventListener('click', function(){
+			app.cargarFoto(camera.PictureSourceType.PHOTOLIBRARY);
+		});
+
 	},
 
 	tomarFoto: function() {
 		var opciones = {
 			quality: 50,
+			sourceType: pictureSourceType,
 			destinationType: Camera.DestinationType.FILE_URI,
 			targetWidth: 300,
 			targetHeight: 300,
@@ -25,12 +45,34 @@ var app = {
 	},
 
 	fotoTomada: function(imageURI) {
-		var image = document.querySelector('#foto');
-		image.src = imageURI;
+		var img = document.createElement('img');
+		img.onload = function () {
+			app.pintarFoto(img);
+		}
+//		var image = document.querySelector('#foto');
+		img.src = imageURI;
+	},
+
+	pintarFoto: function(img) {
+		var canvas = document.querySelector('#foto');
+		var context = canvas.getContext('2d');
+		canvas.width = img.width;
+		canvas.height = img.height;
+		context.drawImage(img, 0, 0, img.width, img.height);
 	},
 
 	errorAlTomarFoto: function(message) {
 		console.log('fallo al tomar foto o toma cancelada: ' + message);
+	},
+
+	aplicaFiltro: function(filterName){
+		var canvas = document.querySelector('#foto');
+		var context = canvas.getContext('2d');
+		imageData = context.getImageData(0, 0, canvas.width ,canvas.height);
+
+		effects[filterName](imageData.data);
+
+		context.putImageData(imageData, 0, 0);
 	}
 };
 
